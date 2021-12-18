331bce49b19c4fde84216d001a10f34dd9eee578
ed-park <pjy20450107@gmail.com>
2021-12-19 05:47:59 +0900

    CalendarRange 구현

---
+++ src/components/Settlement/CalendarRange/styled.tsx
@@ -0,0 +1,123 @@
+import styled from '@styles/theme-components';
+
+export const CalendarHeader = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: end;
+  margin-top: 1rem;
+`;
+
+export const CalendarHeading = styled.h3`
+  font-size: 2.25rem;
+  font-weight: 500;
+`;
+
+export const CalendarMain = styled.div`
+  padding: 32px 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CalendarDayContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 1rem;
+  font-weight: bold;
+  width: 448px;
+`;
+
+export const CaledarDay = styled.div`
+  width: 64px;
+  padding: 1rem 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const CalendarDateContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 448px;
+`;
+
+export const CalendarDateHistory = styled.div`
+  border-top: 1px solid #e5e5e5;
+  margin: 0 64px;
+  padding: 16px 0;
+
+  & > h3 {
+    font-size: 1.25rem;
+    font-weight: bold;
+    margin-bottom: 16px;
+  }
+
+  & > div > p {
+    margin-bottom: 4px;
+  }
+`;
+
+export const CalendarDate = styled.div<{
+  isOtherMonth: boolean;
+  isHistoryExist: boolean;
+  isPrev?: boolean;
+  isNext?: boolean;
+  isBetween?: boolean;
+}>`
+  width: 64px;
+  height: 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => (props.isOtherMonth ? '#cbd1d2' : '#7c8a95')};
+  border: 1px solid transparent;
+  position: relative;
+
+  border-radius: 30%;
+  ${(props) =>
+    props.isHistoryExist &&
+    `&:after {
+     background: #6ca4f4;
+     border-radius: 50%;
+     display: block;
+     content: '';
+     bottom: 6px;
+     left: 50%;
+     height: 8px;
+     margin: -4px 0 0 -4px;
+     position: absolute;
+     width: 8px;
+    }`}
+
+  &:hover {
+    border-color: ${(props) => (props.isOtherMonth ? '#cbd1d2' : '#6CA4F4')};
+  }
+
+  ${(props) =>
+    props.isPrev && `background: #6ca4f4; border-top-right-radius: 0; border-bottom-right-radius: 0; color: white}`}
+  ${(props) =>
+    props.isNext && `background: #6ca4f4; border-top-left-radius: 0; border-bottom-left-radius: 0; color: white}`}
+
+  ${(props) => props.isBetween && `background: ${props.theme.colors.whiteGray}; border: none; border-radius: 0;`}
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+
+  & > button {
+    width: 50%;
+    padding: 1rem 0;
+    font-size: 1rem;
+  }
+
+  & > button:nth-child(1) {
+    border-bottom-left-radius: 1rem;
+    background-color: #e6e5e5;
+  }
+  & > button:nth-child(2) {
+    border-bottom-right-radius: 1rem;
+    background-color: ${(props) => props.theme.colors.main};
+    color: white;
+  }
+`;
