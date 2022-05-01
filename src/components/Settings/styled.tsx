832ec26e846445111303847e9106e343478dbb12
ed-park <pjy20450107@gmail.com>
2022-05-01 17:10:20 +0900

    viewport 스크롤 이슈(가로가 긴) 해결

--- src/components/Settings/styled.tsx
+++ src/components/Settings/styled.tsx
@@ -2,7 +2,7 @@ import styled from '@styles/theme-components';
 
 export const SettingsContainer = styled.div`
   width: 100%;
-  height: calc(100vh - 70px);
+  height: calc(100vh - 102px);
   display: flex;
   flex-direction: column;
   align-items: center;
